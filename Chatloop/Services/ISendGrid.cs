a20803b97cb31372e78e8c1f39eb3b48e6fa5cf3
developerdcs007 <developerdcs007@outlook.com>
2018-10-13 22:49:59 +1000

    Send Grid Email is updated.

--- Chatloop/Services/ISendGrid.cs
+++ Chatloop/Services/ISendGrid.cs
@@ -22,9 +22,9 @@ namespace Chatloop.Services
         {
             var apiKey = "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";
             var client = new SendGridClient(apiKey);
-            var from = new EmailAddress("Devarsh14@gmail.com", "Example User");
+            var from = new EmailAddress("Devarsh14@gmail.com", "Devarsh");
             var subject = "Sending with SendGrid is Fun";
-            var to = new EmailAddress("Devarsh14@gmail.com", "Example User");
+            var to = new EmailAddress("devarsh.shukla@icloud.com", "Arya Shukla");
             var plainTextContent = "and easy to do anywhere, even with C#";
             var htmlContent = "<strong>and easy to do anywhere, even with C#</strong>";
             var msg = MailHelper.CreateSingleEmail(from, to, subject, plainTextContent, htmlContent);
