741ff154d4e0fa45531cc4289fd5656afec0bee1
developerdcs007 <developerdcs007@outlook.com>
2018-10-13 14:35:40 +1000

    datetime added.

---
+++ Chatloop/Models/Reminder.cs
@@ -0,0 +1,21 @@
+using System;
+
+namespace Chatloop.Models
+{
+    public class Reminder
+    {
+        bool RemindOnMonday;
+        DateTime RemindOnMondayTime;
+        bool RemindOnTuesday;
+        DateTime RemindOnTuesdayTime;
+        bool RemindOnWednesday;
+        DateTime RemindOnWednesdayTime;
+        bool RemindOnThursday;
+        DateTime RemindOnThursdayTime;
+        bool RemindOnFriday;
+        DateTime RemindOnFridayTime;
+        bool RemindOnSaturday;
+        DateTime RemindOnSaturdayTime;
+
+    }
+}
